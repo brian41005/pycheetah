bd57d170e7b06050f049b0df3ef9d263f9579255
Brian Ma <brian41005@gmail.com>
2017-12-07 21:34:12 +0800

    using concurrent module

--- example/nytimes.py
+++ example/nytimes.py
@@ -17,7 +17,7 @@ sys.path.append(pkg_dir)
 import pycheetah
 
 
-class DailyPage(pycheetah.Page):
+class DailyPage(pycheetah.Cheetah):
     def request(self, url):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
@@ -43,7 +43,8 @@ class DailyPage(pycheetah.Page):
         return urls
 
 
-class NewsPage(pycheetah.Page):
+class NewsPage(pycheetah.Cheetah):
+
     def request(self, url):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
@@ -79,26 +80,26 @@ class NewsPage(pycheetah.Page):
         return soup.find('link', attrs={'rel': 'canonical'})['href']
 
 
-class DailyPageManager(pycheetah.DefaultTaskManager):
-    __page_class__ = DailyPage
+# class DailyPageManager(pycheetah.DefaultTaskManager):
+#     __page_class__ = DailyPage
 
 
-class NewsPageManager(pycheetah.DefaultTaskManager):
-    __page_class__ = NewsPage
+# class NewsPageManager(pycheetah.DefaultTaskManager):
+#     __page_class__ = NewsPage
 
 
 if __name__ == '__main__':
-    all_daily_urls = list(pycheetah.gen_urls('http://www.nytimes.com/indexes/%s/todayspaper/index.html',
-                                             '2017/1/1', '2017/12/1',
-                                             date_format='%Y/%m/%d',
-                                             product=['date']))
     pycheetah.init_logger()
     ts = time.time()
-    result = pycheetah.start(all_daily_urls, DailyPageManager)
-    items = pycheetah.start(result['urls'], NewsPageManager)
+
+    urls = list(pycheetah.gen_urls('http://www.nytimes.com/indexes/%s/todayspaper/index.html',
+                                   '2017/1/1', '2017/12/1',
+                                   date_format='%Y/%m/%d',
+                                   product=['date']))
+    result = pycheetah.start(urls, DailyPage)
+    # result = pycheetah.start(result['urls'], NewsPageManager)
 
     cost_time = time.time() - ts
     print('time:%.6f, %d data, avg:%.6f' %
-          (cost_time, len(items),
-           cost_time / len(items)))
-    print(items[0])
+          (cost_time, len(result),
+           cost_time / len(result)))
--- pycheetah/task.py
+++ pycheetah/task.py
@@ -1,25 +1,46 @@
+from concurrent import futures
 import logging
+from abc import ABC, ABCMeta, abstractmethod
 from queue import PriorityQueue
+
 from .container import Result
-__all__ = ['TaskManager']
+
+__all__ = ['OldTaskManager', 'DefaultTaskManager']
 
 
-class DefaultTaskManager:
-    __page_class__ = None
+class ABCTaskManager(metaclass=ABCMeta):
+    @abstractmethod
+    def start(self):
+        pass
 
-    def __new__(cls, *args, **kwargs):
-        TaskManager.__page_class__ = cls.__page_class__
-        return super(TaskManager, cls).__new__(cls)
 
-    def __init__(self, urls, num_thread=1):
-        self.queue = PriorityQueue(maxsize=num_thread)
+class DefaultTaskManager(ABCTaskManager):
+    def __init__(self, urls, cheetah, num_thread=1):
         self.urls = urls
-        self.num_thread = num_thread
+        self.cheetah = cheetah
+        self.num_thread = min(len(self.urls), num_thread)
+
+    def start(self):
+        with futures.ThreadPoolExecutor(self.num_thread) as executor:
+            to_do = [executor.submit(self.cheetah('', url))
+                     for url in self.urls]
+            result = [future.result()
+                      for future in futures.as_completed(to_do)]
+        return Result(result)
+
+
+class OldTaskManager(ABCTaskManager):
+
+    def __init__(self, urls, cheetah, num_thread=1):
+        self.urls = urls
+        self.num_thread = min(len(self.urls), num_thread)
+        self.queue = PriorityQueue(maxsize=self.num_thread)
+        self.cheetah = cheetah
 
     def start(self):
         result = []
         for i, url in enumerate(self.urls):
-            newpage = TaskManager.__page_class__(str(i), url)
+            newpage = self.cheetah(str(i), url)
             newpage.start()
             self.queue.put(newpage)
 
@@ -30,7 +51,7 @@ class DefaultTaskManager:
                     temp_result = page.join(timeout=0.5)
                     if temp_result:
                         result.append(temp_result)
-                        # logging.info(page.url)
+                        logging.info(page.url)
                     else:
                         self.queue.put(page)
                         logging.info(page.url + ' [TIMEOUT]')
@@ -41,5 +62,5 @@ class DefaultTaskManager:
         while not self.queue.empty():
             page = self.queue.get()
             result.append(page.join())
-            # logging.info(page.url)
+            logging.info(page.url)
         return Result(result)
