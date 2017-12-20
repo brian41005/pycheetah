99f03e2525835fa5bcbad00d046237dc119f20c7
Brian Ma <brian41005@gmail.com>
2017-12-20 16:14:32 +0800

    move utest

--- tests/test_main.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-if __name__ == '__main__':
-    testsuite = unittest.TestLoader().discover('each_test',  pattern="test*.py")
-    unittest.TextTestRunner(verbosity=3).run(testsuite)
